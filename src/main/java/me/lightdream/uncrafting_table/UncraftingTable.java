5cbdb16b53a7365b87047c992e7aa440a8a1164d
L1ghtDream <62599375+L1ghtDream@users.noreply.github.com>
2021-02-07 21:36:11 +0200

    Added an invetory & an item validator to Power Generator (Coal)

--- src/main/java/me/lightdream/uncrafting_table/UncraftingTable.java
+++ src/main/java/me/lightdream/uncrafting_table/UncraftingTable.java
@@ -1,6 +1,8 @@
 package me.lightdream.uncrafting_table;
 
 import me.lightdream.uncrafting_table.blocks.ModBlocks;
+import me.lightdream.uncrafting_table.blocks.PowerGenerator;
+import me.lightdream.uncrafting_table.blocks.PowerGeneratorTile;
 import me.lightdream.uncrafting_table.items.PortableUncraftingTable;
 import me.lightdream.uncrafting_table.setup.ClientProxy;
 import me.lightdream.uncrafting_table.setup.IProxy;
@@ -9,6 +11,8 @@ import me.lightdream.uncrafting_table.setup.ServerProxy;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -45,6 +49,7 @@ public class UncraftingTable {
         @SubscribeEvent
         public static void onBlockRegistry(final RegistryEvent.Register<Block> event) {
             event.getRegistry().register(new me.lightdream.uncrafting_table.blocks.UncraftingTable());
+            event.getRegistry().register(new PowerGenerator());
         }
 
         @SubscribeEvent
@@ -56,7 +61,13 @@ public class UncraftingTable {
             event.getRegistry().register(new PortableUncraftingTable());
 
             //Block Items
-            event.getRegistry().register(new BlockItem(ModBlocks.UNCRAFTING_TABLE,properties).setRegistryName("uncrafting_table"));
+            event.getRegistry().register(new BlockItem(ModBlocks.UNCRAFTING_TABLE, properties).setRegistryName("uncrafting_table"));
+            event.getRegistry().register(new BlockItem(ModBlocks.POWER_GENERATOR, properties).setRegistryName("power_generator"));
+        }
+
+        @SubscribeEvent
+        public static void onTileEntityRegistry(final RegistryEvent.Register<TileEntityType<?>> event) {
+            event.getRegistry().register(TileEntityType.Builder.create(PowerGeneratorTile::new, ModBlocks.POWER_GENERATOR).build(null).setRegistryName("power_generator"));
         }
     }
 
--- src/main/java/me/lightdream/uncrafting_table/blocks/ModBlocks.java
+++ src/main/java/me/lightdream/uncrafting_table/blocks/ModBlocks.java
@@ -1,9 +1,17 @@
 package me.lightdream.uncrafting_table.blocks;
 
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.registries.ObjectHolder;
 
 public class ModBlocks {
 
     @ObjectHolder("uncrafting_table:uncrafting_table")
     public static UncraftingTable UNCRAFTING_TABLE;
+
+    @ObjectHolder("uncrafting_table:power_generator")
+    public static PowerGenerator POWER_GENERATOR;
+
+    @ObjectHolder("uncrafting_table:power_generator")
+    public static TileEntityType<PowerGeneratorTile> POWER_GENERATOR_TILE;
 }
